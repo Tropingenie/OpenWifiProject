dc6a41ff17f04e77ee13ab54e07bbd726b130514
Pon <steeevie22@gmail.com>
2026-05-06 12:34:19 -0700

    Add wifi connection function

--- main.py
+++ main.py
@@ -91,22 +91,22 @@ def connect_to_ssid(ssid):
 # known_networks = get_known_networks()
 # logger.debug(known_networks)
 
+connect_to_ssid("BeMyGuest")
+exit(0)
+
 with navigate_portal.WebDriver() as driver:
 
     while True:
-        connected = has_internet() # force false for dev testing
+        connected = False # has_internet() # force false for dev testing
         if connected:
             logger.info("Internet connection is up!")
         else:
             logger.info("No internet connection.")
             
+            navigate_portal.CaptivePortalNavigator(driver).navigate(portal="http://1.1.1.1") # Use an http IP to trigger captive portal
             for ssid, is_open in get_ssids().items():
                 logger.debug(f"{ssid} is {'open' if is_open else 'secure'}")
-                if is_open:
+                if is_open():
                     connect_to_ssid(ssid)
-                    if not has_internet():
-                        navigate_portal.CaptivePortalNavigator(driver).navigate(portal="http://1.1.1.1") # Use an http IP to trigger captive portal
-                    if has_internet():
-                        break
-        sleep(5)
-        #input("Press enter to run next cycle") # manual run for debug
+        #sleep(5)
+        input("Press enter to run next cycle") # manual run for debug

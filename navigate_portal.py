0b048d2b7e5cb6a2939051a102aa2132329d51ea
Pon <steeevie22@gmail.com>
2026-05-11 12:19:54 -0700

    Refactor to use Playwright
    Playwright + Webdriver works better for this. It includes a built
    in script generator, and runs significantly more performant on
    the Pi (Selenium was bringing the Zero W 2 to its knees even
    headless).

--- navigate_portal.py
+++ navigate_portal.py
@@ -1,16 +1,14 @@
 import logging
 import os
+import re
 from contextlib import contextmanager
 from subprocess import run
 from time import sleep
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import NoSuchDriverException, ElementNotInteractableException, InvalidArgumentException
+from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright._impl._errors import TimeoutError
 
 ACCEPT_TEXT = ["accept", "connect", "agree", "continue", "submit", "internet", "access", "online"]
-GECKO_DRIVER = os.path.abspath("./geckodriver")
 
 logger = logging.getLogger(__name__)
 
@@ -18,22 +16,23 @@ logger = logging.getLogger(__name__)
 def WebDriver():
     driver = None
     try:
-        service = Service(executable_path=GECKO_DRIVER)
-        driver = webdriver.Firefox(service=service)
-        yield driver
-    except NoSuchDriverException as e:
+        with sync_playwright() as playwright:
+            driver = playwright
+            yield playwright
+    except Exception as e:
         logger.error(e)
         logger.info("""If on Pi, try: 
-        wget https://www.github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz
-        tar -xf geckodriver-v0.36.0-linux-aarch64.tar.gz""")
-        exit(1)
-    finally:
-       if driver is not None:
-           driver.quit()
+        playwright install-deps
+        playwright install webkit""")
+        raise
 
 class CaptivePortalNavigator:
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, playwright=None):
+        if playwright is None:
+            with WebDriver() as p:
+                self.playwright = p
+        else:
+            self.playwright = playwright
 
     def navigate(self, portal=None, script=None):
         """
@@ -48,8 +47,12 @@ class CaptivePortalNavigator:
         """
         Automatically navigate captive portal with a known url, trying a variety of common flows
         """
-        self.driver.get(portal)
-        self.driver.implicitly_wait(1)
+
+        browser = self.playwright.webkit.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("portal")
+
         # Simple algorithm:
         #    1. Look for and tick any checkboxes
         #    2. Look for and fill any text inputs with "name" or "email" in the placeholder or label
@@ -58,6 +61,10 @@ class CaptivePortalNavigator:
         self._check_boxes()
         self._fill_inputs()
         self._click_buttons()
+            
+        input("Debug: Press enter to close Playwright.")
+        context.close()
+        browser.close()
 
     def _navigate_script(self, script):
         """
@@ -67,63 +74,30 @@ class CaptivePortalNavigator:
             - selector: CSS selector for the element to interact with
             - value: (for input) the value to input
         """
-        for step in script:
-            if step["action"] == "click":
-                element = self.driver.find_element(By.CSS_SELECTOR, step["selector"])
-                element.click()
-            elif step["action"] == "input":
-                element = self.driver.find_element(By.CSS_SELECTOR, step["selector"])
-                element.send_keys(step["value"])
-            else:
-                logger.error(f"Unknown action {step['action']} in script")
+        raise NotImplementedError
 
     def _check_boxes(self):
-        checkboxes = self.driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
-        for checkbox in checkboxes:
-            if not checkbox.is_selected():
-                checkbox.click()
+        raise NotImplementedError
 
     def _fill_inputs(self):
-        inputs = self.driver.find_elements(By.CSS_SELECTOR, "input")
-        for input in inputs:
-            input_type = input.get_attribute("type").lower()
-            try:
-                if "email" in input_type:
-                    input.send_keys("test@example.com")
-                elif "name" in input_type:
-                    input.send_keys("Test User")
-                else:
-                    input.send_keys("test")
-            except ElementNotInteractableException:
-                logger.debug(f"Input {input} not interactable, skipping")
-            except InvalidArgumentException:
-                logger.debug(f"Input {input} not a text field, skipping")
-            else:
-                self.driver.implicitly_wait(1)
+        raise NotImplementedError
 
     def _click_buttons(self):
         buttons = self.driver.find_elements(By.CSS_SELECTOR, "button")
         links = self.driver.find_elements(By.CSS_SELECTOR, "a")
-        for button in buttons:
-            if self._accept_text_in(button.text.lower()):
-                button.click()
-                return
-        for link in links:
-            if self._accept_text_in(link.text.lower()):
-                link.click()
-                return
+        links = page.get_by_role("link", name=re.compile("|".join(ACCEPT_TEXT), re.IGNORECASE))
+        try:
+            for loc in links.all().extend(buttons.all()):
+                loc.click(timeout=500)
+        except TimeoutError:
+            pass # expected if the first locator works
 
-    def _accept_text_in(self, text):
-        for accept_text in ACCEPT_TEXT:
-            if accept_text in text:
-                return True
-        return False
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     with WebDriver() as driver:
         navigator = CaptivePortalNavigator(driver)
-        # navigator.navigate(portal="file:///" + os.path.join(os.getcwd(), "test", "aandw.html"))
-        navigator.navigate(portal="https://www.selenium.dev/selenium/web/web-form.html")
+        navigator.navigate(portal="file:///" + os.path.join(os.getcwd(), "test", "aandw.html"))
+        #navigator.navigate(portal="https://www.selenium.dev/selenium/web/web-form.html")
         input()

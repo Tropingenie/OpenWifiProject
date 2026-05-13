19a9209de72a0ba52a39d584b529fc40aa7f7240
Pon <steeevie22@gmail.com>
2026-05-13 12:48:37 -0700

    Working at A&W
      - Test run at A&W
      - Add debug control for unit testing navigate_portal

--- navigate_portal.py
+++ navigate_portal.py
@@ -6,9 +6,12 @@ from subprocess import run
 from time import sleep
 
 from playwright.sync_api import Playwright, sync_playwright, expect
-from playwright._impl._errors import TimeoutError
+from playwright._impl._errors import TimeoutError, Error
 
 ACCEPT_TEXT = ["accept", "connect", "agree", "continue", "submit", "internet", "access", "online"]
+EMAIL_TEXT = ["email"]
+PASSWORD_TEXT = ["password"]
+NAME_TEXT = ["name"]
 
 logger = logging.getLogger(__name__)
 
@@ -58,29 +61,55 @@ class CaptivePortalNavigator:
         #    2. Look for and fill any text inputs with "name" or "email" in the placeholder or label
         #    3. Look for and click any buttons with "accept" or "connect" in the text
         #    4. Profit
-        #self._check_boxes()
-        #self._fill_inputs()
+        if __name__ == "__main__":
+            input("Page loaded")
+        self._check_boxes()
+        if __name__ == "__main__":
+            input("Boxes checked")
+        self._fill_inputs()
+        if __name__ == "__main__":
+            input("Inputs filled")
         self._click_buttons()
+        if __name__ == "__main__":
+            input("Buttons clicked")
             
-        input("Debug: Press enter to close Playwright.")
+            input("Debug: Press enter to close Playwright.")
         context.close()
         browser.close()
 
     def _navigate_script(self, script):
-        """
-        Navigate captive portal using provided script, which is a list of steps to execute
-        Each step is a dict with keys:
-            - action: "click" or "input"
-            - selector: CSS selector for the element to interact with
-            - value: (for input) the value to input
-        """
         raise NotImplementedError
 
     def _check_boxes(self):
-        raise NotImplementedError
+        check_boxes = self.page.get_by_role("checkbox")
+        radio_buttons = self.page.get_by_role("radio")
+        locators = []
+        if check_boxes is not None:
+            locators.extend(check_boxes.all())
+        if radio_buttons is not None:
+            locators.extend(radio_buttons.all())
+
+        for loc in locators:
+            if not loc.is_checked():
+                loc.click(timeout=500)
 
     def _fill_inputs(self):
-        raise NotImplementedError
+        email_inputs = self.page.get_by_role("textbox", name=re.compile("|".join(EMAIL_TEXT), re.IGNORECASE))
+        name_inputs = self.page.get_by_role("textbox", name=re.compile("|".join(NAME_TEXT), re.IGNORECASE))
+        password_inputs = self.page.get_by_role("textbox", name=re.compile("|".join(PASSWORD_TEXT), re.IGNORECASE))
+        all_inputs = self.page.get_by_role("textbox")
+        for loc in all_inputs.all():
+            try:
+                if loc in email_inputs.all():
+                    loc.fill("email@domain.com", timeout=500)
+                elif loc in name_inputs.all():
+                    loc.fill("name", timeout=500)
+                elif loc in password_inputs.all():
+                    pass # assume no password (and fail if there is one)
+                else:
+                    loc.fill("lorem ipsum dolor", timeout=500)
+            except (TimeoutError, Error):
+                pass # expected, e.g. if element is not interactable
 
     def _click_buttons(self):
         links = self.page.get_by_role("link", name=re.compile("|".join(ACCEPT_TEXT), re.IGNORECASE))
@@ -102,6 +131,6 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     with WebDriver() as driver:
         navigator = CaptivePortalNavigator(driver)
-        navigator.navigate(portal="file:///" + os.path.join(os.getcwd(), "test", "aandw.html"))
-        #navigator.navigate(portal="https://www.selenium.dev/selenium/web/web-form.html")
-        input()
+        #navigator.navigate(portal="file:///" + os.path.join(os.getcwd(), "test", "aandw.html"))
+        navigator.navigate(portal="https://www.selenium.dev/selenium/web/web-form.html")
+        input("press enter to exit")

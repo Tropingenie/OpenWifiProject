637b37cbe145fd1f999810adddf06c2d35d219d4
Tropingenie <steeevie22@gmail.com>
2026-07-31 13:31:00 -0700

    Begin refactor to more robust generic login
      - Navigate URLs with page.goto() to avoid click timeout issue
      - Beginnings of JS redirect hydration handling
      - Commented out code until core features are reliable

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ from time import sleep
 from tabulate import tabulate
 
 import navigate_portal
+from shared import nmcli_lock
+
+from test import auto_login
 
 LOG_LEVEL = logging.DEBUG
 FORCE_NO_INTERNET = False
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s - %(levelname)s - %(message)s')
 
-MIN_SIG_STRENGTH = 33
-CONNECTION_TIMEOUT = 10 # seconds to wait for nmcli conn to finish
+MIN_SIG_STRENGTH = 50
+CONNECTION_TIMEOUT = 30 # seconds to wait for nmcli conn to finish
 POLL_RATE_LONG = 5 # seconds to wait between checks when you have internet
 POLL_RATE_SHORT = 1 # seconds to wait between checks when without internet
-PING_TIMEOUT = 0.5 # 500 ms
+PING_TIMEOUT = 5 # 5000 ms
 
 # Interface names
 IFNAME_1 = "wlan0" # default pi NIC, use this  as hotspot so we can ssh without internet
@@ -58,41 +61,69 @@ def has_internet():
         logger.debug(e)
         return False
     if len(ping_return.stdout) > 0:
-        logger.debug(ping_return.stdout)
+        logger.debug(ping_return.stdout.strip())
     if len(ping_return.stderr) > 0:
-        logger.debug(ping_return.stderr)
-    if "1 packets transmitted, 1 received" in ping_return.stdout:
-        return True
-    elif "1 packets transmitted, 0 received" in ping_return.stdout or "Network is unreachable" in ping_return.stderr:
-        return False
-    else:
-        assert False, f"ping returning unexpected output: \nstdout: {ping_return.stdout}\n\nstderr: {ping_return.stderr}"
+        logger.debug(ping_return.stderr.strip())
+    def check_curl():
+        try:
+            curl_return = run("curl networkcheck.kde.org", shell=True, capture_output=True, text=True, timeout=PING_TIMEOUT)
+            logger.debug(f"curl returned returncode: {curl_return.returncode} with page contents:" + (f"\n{curl_return.stdout.strip()}" if len(curl_return.stdout) > 0 else ""))
+            return curl_return.returncode == 0 and curl_return.stdout.strip() == "OK"
+        except (TimeoutError, TimeoutExpired) as e:
+            logger.debug(e)
+            return False
+    logger.debug(f"ping returned returncode: {ping_return.returncode}")
+    curl_result = check_curl()
+    return (ping_return.returncode == 0) and curl_result
 
 def get_ssids():
-    nmcli_return =  run(f"nmcli -t -f \"SSID,SECURITY,SIGNAL\" device wifi list --rescan yes ifname {IFNAME_2}", shell=True, capture_output=True, text=True)
-    logger.debug(nmcli_return.stdout)
+    logger.debug("Fetching ssids")
+    unique_ssids = set()
+    nmcli_return =  run(f"nmcli -t -f \"SECURITY,SIGNAL,SSID\" device wifi list --rescan yes ifname {IFNAME_2}", shell=True, capture_output=True, text=True)
+    if len(nmcli_return.stdout) > 0:
+        logger.debug(nmcli_return.stdout.strip())
+    if len(nmcli_return.stderr) > 0:
+        logger.error(nmcli_return.stderr.strip())
+    if nmcli_return.returncode != 0:
+        logger.error("Error while fetching ssids")
+        return
+
     for line in nmcli_return.stdout.splitlines():
         logger.debug(f"Scanning line: {line}")
-        ssid, security, signal = line.split(':')
+        try:
+            security, signal, ssid = line.split(':', 3)
+        except ValueError as e:
+            # Workaround for when ':' appears in the SSID
+            # SSID spec allows for any unicode so string parsing should be removed
+            logger.error(f"Intercepted ValueError when parsing {line}")
+            logger.error(e)
+            pass
+
         signal = int(signal) # strtoi
         logger.debug(f"Found network\n\tname     = {ssid}\n\tsignal   = {signal}\n\tsecurity = {security}")
-        if signal < MIN_SIG_STRENGTH: # nmcli returns sorted in order of signal strength
-            break
-        elif ssid == "": # hidden network
-            continue
-        else:
-            yield ssid, (security == "") # True if open network
+        if ssid not in unique_ssids:
+            unique_ssids.add(ssid)
+            if signal < MIN_SIG_STRENGTH: # nmcli returns sorted in order of signal strength
+                break
+            elif ssid == "": # hidden network
+                continue
+            else:
+                yield ssid, (security == "") # True if open network
+
+    if len(unique_ssids) < 1:
+        logging.warning("No SSIDs found! WLAN1 may be broken.")
+    else:
+        logging.debug(f"Parsed {len(unique_ssids)} unique ssids\n{unique_ssids}")
+
 
 ssid_list = []
 
-def connect_to_ssid(ssid, known=False):
+def connect_to_ssid(ssid):
     global ssid_list
-    if known:
-        conn_attempt_return = run(f"nmcli conn up {ssid}", shell=True, capture_output=True)
-        conn_attempt_return.check_returncode()
-        return
+    global nmcli_lock
     try:
-        conn_attempt_return = run(f"nmcli d wifi connect '{ssid}' ifname {IFNAME_2}", shell=True, capture_output=True, text=True, timeout=CONNECTION_TIMEOUT)
+        with nmcli_lock:
+            conn_attempt_return = run(f"nmcli d wifi connect '{ssid}' ifname {IFNAME_2}", shell=True, capture_output=True, text=True, timeout=CONNECTION_TIMEOUT)
         ssid_list.append(ssid)
     except (TimeoutError, TimeoutExpired) as e:
         logger.warning(f"Timed out while connecting to {ssid}.")
@@ -101,23 +132,21 @@ def connect_to_ssid(ssid, known=False):
     logger.debug(f"\"{conn_attempt_return.args}\" returned {conn_attempt_return.returncode}")
     logger.info(f"{conn_attempt_return.stdout[5:-1]}") # Slice list to strip ANSI terminal codes
     if conn_attempt_return.returncode != 0:
-        logger.error(f"{conn_attempt_return.stderr}")
+        logger.error(f"{conn_attempt_return.stderr.strip()}")
     return conn_attempt_return.returncode
 
-known_networks = None
 def cleanup_ssids():
-    assert known_networks is not None, "Attemped to clean ssids without first populating known_networks"
     global ssid_list
     for ssid in ssid_list:
-        if ssid not in known_networks:
-            cleanup_return = run(f"nmcli connection delete id '{ssid}'", shell=True, capture_output=True)
-            cleanup_return.check_returncode()
+        cleanup_return = run(f"nmcli connection delete id '{ssid}'", shell=True, capture_output=True)
+        cleanup_return.check_returncode()
     ssid_list.clear()
 
 def get_known_networks():
     nmcli_return = run("nmcli -t -g \"NAME\" conn show", shell=True, text=True, capture_output=True)
     logger.info("Pulling known SSID list")
-    logger.debug(nmcli_return.stdout + nmcli_return.stderr)
+    if len(nmcli_return.stdout + nmcli_return.stderr) > 0:
+        logger.debug(nmcli_return.stdout + nmcli_return.stderr.strip())
     return nmcli_return.stdout
 
 # ssids = get_ssids()
@@ -126,34 +155,41 @@ def get_known_networks():
 known_networks = get_known_networks()
 # logger.debug(known_networks)
 
-
-with navigate_portal.WebDriver() as driver:
-    while True:
-        # dev switch for debugging
-        connected = has_internet() and not FORCE_NO_INTERNET
-
-        if connected:
-            internet_check_interval = POLL_RATE_LONG
-            logger.info("Internet connection is up!")
-        else:
-            logger.info("No internet connection.")
-            cleanup_ssids()
-            internet_check_interval = POLL_RATE_SHORT
-            for ssid, is_open in get_ssids():
-                logger.debug(f"{ssid} is {'open' if is_open else 'secure'}")
-                if is_open:
-                    if connect_to_ssid(ssid) == 0: # non zero return code indicates error
-                        if not has_internet():
-                            navigate_portal.CaptivePortalNavigator(driver).navigate(portal="http://1.1.1.1") # Use an http IP to trigger captive portal
-                            if has_internet():
-                                break
-                            elif LOG_LEVEL == logging.DEBUG:
-                                logger.error("Portal navigation failed.")
-                                input("Press enter to continue.")
-                        else:
-                            break
+def main():
+#    try:
+#        with navigate_portal.WebDriver() as driver:
+    if True:
+        if True:
+            while True:
+                # FORCE_NO_INTERNET is a dev switch for debugging
+                connected = has_internet() and not FORCE_NO_INTERNET
+                if connected:
+                    internet_check_interval = POLL_RATE_LONG
+                    logger.info("Internet connection is up!")
                 else:
-                    if(ssid in known_networks and connect_to_ssid(ssid, known=True) == 0 and has_internet()):
-                        break
-        sleep(internet_check_interval)
-        #input("Press enter to run next cycle") # manual run for debug
+                    logger.info("No internet connection.")
+                    auto_login()
+#                    cleanup_ssids()
+#                    internet_check_interval = POLL_RATE_SHORT
+#                    for ssid, is_open in get_ssids():
+#                        logger.debug(f"{ssid} is {'open' if is_open else 'secure'}")
+#                        if (is_open and connect_to_ssid(ssid) == 0 and not has_internet()):
+#                            navigate_portal.CaptivePortalNavigator(driver).navigate(portal="http://1.1.1.1") # Use an http IP to trigger captive portal
+#                            if has_internet():
+#                                break
+#                            elif LOG_LEVEL == logging.DEBUG:
+#                                logger.error("Portal navigation failed.")
+#                                input("Press enter to continue.")
+#                        elif has_internet(): # check is quick enough that doing it twice isn't a problem
+#                            break
+#                        elif(ssid in known_networks and connect_to_ssid(ssid) == 0 and has_internet()):
+#                            break
+                logger.debug("Scan cycle finished")
+                sleep(internet_check_interval)
+                #input("Press enter to run next cycle") # manual run for debug
+#    finally:
+#        cleanup_ssids()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,62 @@
+import re
+
+from playwright.sync_api import sync_playwright
+
+
+def auto_login():
+    with sync_playwright() as p:
+        browser = p.webkit.launch()
+        context = browser.new_context(
+            ignore_https_errors=True
+        )
+        page = context.new_page()
+
+        page.route(
+            "**/*.{png,jpg,jpeg,gif,svg,css,font}",
+            lambda route: route.abort()
+        )
+
+        print("nav to networkcheck...")
+        page.goto("http://networkcheck.kde.org", wait_until="networkidle")
+#       ok = page.get_by_text("OK")
+#       print(ok is not None)
+        print(page.content())
+        with open("portal.html", "w") as file:
+            file.write(page.content())
+        #page.screenshot(path="portal.png", full_page=True)
+        if "ftgauth" in page.content():
+            # fortinet debug stuff
+            print(f"current url: {page.url}")
+            print(f"waiting for '**/fgtauth*'")
+            try:
+                page.wait_for_url("**/fgtauth*") # matches the fortigate redirect
+            except Exception as e:
+                print(e)
+            print(f"current url: {page.url}")
+            print(page.content())
+            # end fortinet debug stuff
+
+            # fortigate portal
+            print("looking for button...")
+            link_locator = page.get_by_role("button", name="agree").first
+            link_locator.click(no_wait_after=True)
+        else:
+            # a&w portal
+            print("looking for link...")
+            link_locator = page.get_by_role("link", name="accept").first
+            url = link_locator.get_attribute("href")
+            try:
+                page.goto(url, timeout=3000)
+            except Exception as e:
+                print(e)
+
+        page.wait_for_timeout(3000) # 3s wait for connection to stabilise
+
+        # Verify internet access by probing a simple lightweight endpoint
+        try:
+            response = page.request.get("http://connectivitycheck.gstatic.com/generate_204", timeout=5000)
+            if response.status == 204:
+                print("[+] Internet connection verified!")
+        except Exception as e:
+            print(e)
+            print("[-] Check internet status manually.")
